6aefba4bc9c68a10ab1bf5e44c2dcb367889c532
Chaitanya Makala <chaitanya.m.1996@gmail.com>
2022-11-11 16:37:51 +0530

    created leaderboard

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let score1=0;
+let score2=0;
+updateTable();
+
 document.querySelector(".img1").setAttribute("src", "./images/6.png");
 document.querySelector(".img2").setAttribute("src", "./images/6.png");
 let dice1 = Math.ceil(Math.random() * 6);
@@ -7,9 +11,28 @@ let src2 = "./images/" + dice2 + ".png";
 document.querySelector(".img1").setAttribute("src", src1);
 document.querySelector(".img2").setAttribute("src", src2);
 if (dice1 === dice2) {
-  document.querySelector("h1").textContent = "Draw!";
+  document.querySelector("h2").textContent = "Draw!";
 } else if (dice1 > dice2) {
-  document.querySelector("h1").textContent = "Player 1 Wins!";
+  document.querySelector("h2").textContent = "Player 1 Wins!";
+  let temp = JSON.parse(localStorage.getItem('score1'))||0;
+  temp++;
+  localStorage.setItem('score1',JSON.stringify(temp));
+  updateTable();  
 } else {
-  document.querySelector("h1").textContent = "Player 2 Wins!";
+  document.querySelector("h2").textContent = "Player 2 Wins!";
+  let temp = JSON.parse(localStorage.getItem('score2'))||0;
+  temp++;
+  localStorage.setItem('score2',JSON.stringify(temp));
+  updateTable();
 }
+function updateTable(){
+  document.querySelector('#score2').innerText= JSON.parse(localStorage.getItem('score2'))||0;
+  document.querySelector('#score1').innerText=JSON.parse(localStorage.getItem('score1'))||0;
+}
+document.querySelector('table+p').addEventListener('click',function(){
+  localStorage.setItem('score1',0);
+  localStorage.setItem('score2',0);
+  updateTable();
+})
+
+
